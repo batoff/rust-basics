073e35414e16f77e3a53846d90508c8167053951
wwawron <wojciech.wawron@gmail.com>
2022-07-30 17:08:20 +0200

    Define and make a struct

--- point/point.rs
+++ point/point.rs
@@ -1,3 +1,8 @@
+struct Point {
+    x: i32,
+    y: i32,
+}
+
 fn max(a: i32, b: i32) -> i32 {
     if a > b {
         a
@@ -10,4 +15,11 @@ fn max(a: i32, b: i32) -> i32 {
 fn main() {
     let max_num = max(10, 412);
     println!("{max_num}");
+
+    let p =  Point {
+        x: 3,
+        y: -6,
+    };
+    println!("x: {}, y: {}", p.x, p.y);
 }
+
